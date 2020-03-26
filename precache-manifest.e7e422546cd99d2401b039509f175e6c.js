9c0fdb11a8ccf7ce9e428d7a62c820d2493ecc53
endayusma <gezaramdaciptaan@gmail.com>
2020-03-27 00:23:31 +0700

    Updates

---
+++ precache-manifest.e7e422546cd99d2401b039509f175e6c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "359f5bde08a08d343ea36b158c267b90",
+    "url": "/Tugas-4-WEB/index.html"
+  },
+  {
+    "revision": "401c0d95a595edb4175f",
+    "url": "/Tugas-4-WEB/static/css/2.1bd2ab2f.chunk.css"
+  },
+  {
+    "revision": "81c58a757ecef25a7ff3",
+    "url": "/Tugas-4-WEB/static/css/main.d1c6881f.chunk.css"
+  },
+  {
+    "revision": "401c0d95a595edb4175f",
+    "url": "/Tugas-4-WEB/static/js/2.8113d7c8.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/Tugas-4-WEB/static/js/2.8113d7c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81c58a757ecef25a7ff3",
+    "url": "/Tugas-4-WEB/static/js/main.fcb79f4f.chunk.js"
+  },
+  {
+    "revision": "68ff8f421a38fa60e491",
+    "url": "/Tugas-4-WEB/static/js/runtime-main.e373c869.js"
+  },
+  {
+    "revision": "d1ae1327c3cdd00b3ff3c4c0b8a7bc7f",
+    "url": "/Tugas-4-WEB/static/media/picture.d1ae1327.svg"
+  }
+]);
